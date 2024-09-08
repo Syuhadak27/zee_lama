4f86f12b9e79c19c3620d280c480e928d1bd321d
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2024-09-08 18:32:32 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -318,7 +318,7 @@ if len(BASE_URL) == 0:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://gitlab.com/Syuhadak27/zee_old'
+    UPSTREAM_REPO = 'https://github.com/Syuhadak27/zee_lama'
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
     UPSTREAM_BRANCH = 'upstream'
